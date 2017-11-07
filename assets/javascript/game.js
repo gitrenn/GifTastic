1a7023a0171239e2cabf372e5beb73c888ea2417
Ren <rjingsiu@gmail.com>
2017-11-06 19:53:33 -0800

    got still gifs using ajax get method from giphy

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,51 @@
 $(document).ready(function(){
-    var robots = ["calculon", "daleks", "wall-e", "number six"];
+    var robots = ["calculon", "daleks", "wall-e", "cylon centurion"];
+    var limit = 10; 
 
     renderButtons();
+
+    $(document).on("click", ".robot", displayRobots);
+
+    function displayRobots(){
+        var $robot = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=IBJnoLKIE0W4kb6Uh1VcfNBwXYiiTaRL&q=" + 
+            $robot + "&limit=" + 
+            limit + "&offset=0&rating=G&lang=en";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).done(function(response){
+            var results = response.data;
+            console.log(results);
+            for(var i = 0; i < results.length; i++){
+                var $gifDiv = $("<div>").addClass("item");
+
+                var rating = results[i].rating;
+
+                var $p =$("<p>").text("Rating: " + rating);
+
+                var $image = $("<img>");
+                // get urls for still and animated gifs
+                var img_still_url = results[i].images.fixed_height_still.url;
+                var img_animated_url = results[i].images.fixed_height.url;
+
+                // add attributes for the image tag
+                $image.attr("src", img_still_url);
+                $image.attr("data-still", img_still_url);
+                $image.attr("data-animated", img_animated_url);
+                $image.attr("data-state", "still");
+                $image.addClass("gif");
+
+                $gifDiv.prepend($p);
+                $gifDiv.prepend($image);
+
+                $(".giphy-view").prepend($gifDiv);
+            }
+        });
+    }
+
+
 
 
     function renderButtons(){
@@ -21,4 +65,6 @@ $(document).ready(function(){
             $("#buttons-view").append("|").append($btn);  
         }
     }
+
+
 })
